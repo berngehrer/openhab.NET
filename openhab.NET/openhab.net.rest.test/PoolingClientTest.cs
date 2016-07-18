1d68f3a52bface04560bc20b051e6e929c2113c6
Marco Berngehrer <m.berngehrer@reply.de>
2016-07-18 13:56:04 +0200

    test on requestclient

---
+++ openhab.NET/openhab.net.rest.test/PoolingClientTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace openhab.net.rest.test
+{
+    [TestClass]
+    public class PoolingClientTest
+    {
+        [TestMethod, Timeout(TestTimeout.Infinite)]
+        public void TestRequestClientPooling()
+        {
+            const string url = @"http://192.168.178.69:8080/rest/items";
+            //const string url = @"http://192.168.178.69:8080/rest/items/gLight";
+            //const string url = @"http://192.168.178.69:8080/rest/items/MQTT_TVLED_POW";
+            //const string url = @"http://192.168.178.69:8080/rest/items/MQTT_TVLED_POW/state";
+
+            Task.Run(async () =>
+            {
+                using (var client = new RequestClient())
+                {
+                    var json = await client.GetJson(url, longPooling: true);
+                    Assert.IsFalse(string.IsNullOrEmpty(json));
+                }
+            })
+            .GetAwaiter().GetResult();
+        }
+
+
+        // - As String
+        // - As Date
+        // - As Float
+        // - As Int
+        // - As Bool
+    }
+}
--- openhab.NET/openhab.net.rest/RequestClient.cs
+++ openhab.NET/openhab.net.rest/RequestClient.cs
@@ -2,30 +2,34 @@
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace openhab.net.rest
 {
     internal sealed class RequestClient : IDisposable
     {
-        public async Task<string> GetJson(string url)
+        public async Task<string> GetJson(string url, bool longPooling = false)
         {
-            using (var client = new HttpClient())
+            using (var client = CreateClient(longPooling))
+            using (var response = await client.GetAsync(url))
+            using (var body = await response.Content.ReadAsStreamAsync())
+            using (var reader = new StreamReader(body))
             {
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                //client.DefaultRequestHeaders.Add("X-Atmosphere-Transport", "long-polling");
-                //client.DefaultRequestHeaders.Add("X-Atmosphere-tracking-id", "1234");
-                //client.Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite);
-
-                using (var response = await client.GetAsync(url))
-                {
-                    using (var body = await response.Content.ReadAsStreamAsync())
-                    using (var reader = new StreamReader(body))
-                    {
-                        return reader.ReadToEnd();
-                    }
-                }
+                return reader.ReadToEnd();
             }
+        }
+
+        HttpClient CreateClient(bool longPooling)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            if (longPooling) {
+                client.DefaultRequestHeaders.Add("X-Atmosphere-Transport", "long-polling");
+                client.DefaultRequestHeaders.Add("X-Atmosphere-tracking-id", "1234");
+                client.Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite);
+            }
+            return client;
         }
 
         public void Dispose()
